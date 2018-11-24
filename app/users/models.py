35369ae650ffa063a1e59f02413d462d41150d20
Mirim_Lee <leemirim9654@gmail.com>
2018-11-25 03:27:25 +0900

    User 모델, Track 모델 생성

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """ User Model """
+
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
