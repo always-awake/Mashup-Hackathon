d297ebac3df3e662f42d3c6ba990eef1705f6007
always-awake <leemirim9654@gmail.com>
2019-01-18 00:06:54 +0900

    tracks 앱의 models.py 작성
    - TimeStampedModel 생성(파일 생성시간, 수정 시간 자동 저장을 위함)
    - Track model 수정 및 creator 필드 추가
    - Track_Like 모델 생성

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,11 +1,55 @@
 from django.db import models
+from app.users import models as user_models
 
 
-class Track(models.Model):
+class TimeStampedModel(models.Model):
+
+    """ Base Model """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Track(TimeStampedModel):
 
     """ Track Model """
 
     title = models.CharField(max_length=50)
     track = models.FileField(upload_to='tracks')
+    creator = models.ForeignKey(
+        user_models.User,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='tracks'
+    )
 
-    
+    @property
+    def track_like_count(self):
+        return self.track_likes.count()
+
+    def __str__(self):
+        return '{} - {}'.format(self.title, self.creator.name)
+
+
+class Track_Like(models.Model):
+
+    """ Track Like Model """
+
+    creator = models.ForeignKey(
+        user_models.User,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='track_likes'
+    )
+    track = models.ForeignKey(
+        Track,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='track_likes'
+    )
+
+    def __str__(self):
+        return 'Liked Track: {}'.format(self.track.title)
