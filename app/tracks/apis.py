828f232899e1d56fd833102abb134421aa3259bd
always-awake <leemirim9654@gmail.com>
2019-02-20 00:00:36 +0700

    Serializer, APIView를 사용해 Track의 List, Create & TrackLike의 Create 기능 구현

--- app/tracks/apis.py
+++ app/tracks/apis.py
@@ -1,6 +1,60 @@
-class TrackListAPIView:
-    pass
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import models, serializers
 
 
-class TrackCreateAPIView:
-    pass
+class TrackListAPIView(APIView):
+
+    def get(self, request, format=None):
+
+        tracks = models.Track.objects.all()
+
+        serializer = serializers.TrackSerializer(tracks, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class TrackCreateAPIView(APIView):
+
+    def post(self, request, format=None):
+
+        user = request.user
+
+        serializer = serializers.TrackCreateSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TrackLikeCreateAPIView(APIView):
+
+    def post(self, request, track_id, format=None):
+
+        user = request.user
+
+        try:
+            found_track = models.Track.objects.get(id=track_id)
+        except models.Track.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisiting_like = models.TrackLike.objects.get(
+                user=user,
+                track=found_track
+            )
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+        except models.TrackLike.DoesNotExist:
+            new_like = models.TrackLike.objects.create(
+                user=user,
+                track=found_track
+            )
+            new_like.save()
+            return Response(status=status.HTTP_201_CREATED)
--- app/tracks/serializers.py
+++ app/tracks/serializers.py
@@ -1,6 +1,19 @@
-class TrackSerializer:
-    pass
+from rest_framework import serializers
+from . import models
 
 
-class TrackCreateSerializer:
-    pass
+class TrackSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Track
+        fields = '__all__'
+
+
+class TrackCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Track
+        fields = (
+            'title',
+            'track',
+        )
