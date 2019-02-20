e1727a6747d13b76311e1987efcee2ca0be839f8
always-awake <leemirim9654@gmail.com>
2019-02-20 15:09:35 +0700

    INSTALLED_APPS에 'rest_framework' 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # REST framework
+    'rest_framework',
     # sources app
     'sources',
     # tracks app
